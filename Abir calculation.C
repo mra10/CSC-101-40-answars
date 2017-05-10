e91332ba63f13f6da771fc745a067b7854d7a2a7
mra10 <deathman.mushfiqur@gmail.com>
2017-05-10 21:28:58 +0600

    Add files via upload

---
+++ Abir calculation.C
@@ -0,0 +1,13 @@
+/* program to formula calculation */
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+int a;
+int b;
+printf("enter the number=");
+scanf("%d", &a);
+b=a+1;
+printf("the number is= %d", b);
+getch();
+}
